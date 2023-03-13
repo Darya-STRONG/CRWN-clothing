0025bc82e92b4c64065815e40764209cf1772a0e
Bakulina Daria <dbakulina0404@gmail.com>
2023-03-13 18:43:09 +0200

    adding styled-components and modifying navigation

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -10,7 +10,7 @@ import { CartContext } from '../../contexts/cart.context';
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 
-import './navigation.styles.scss';
+import {NavigationContainer, NavLinks, NavLink, LogoContainer} from './navigation.styles';
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
@@ -18,32 +18,34 @@ const Navigation = () => {
 
   return (
     <Fragment>
-      <div className='navigation'>
-        <Link className='logo-container' to='/'>
-          <CrwnLogo className='logo' />
-        </Link>
-        <div className='nav-links-container'>
-          <Link className='nav-link' to='/shop'>
-            SHOP
-          </Link>
-
-          {currentUser ? (
-            <span className='nav-link' onClick={signOutUser}>
-              {' '}
-              SIGN OUT{' '}
-            </span>
-          ) : (
-            <Link className='nav-link' to='/auth'>
-              SIGN IN
-            </Link>
-          )}
-          <CartIcon/>
-        </div>
-        {isCartOpen && <CartDropdown/>}
-      </div>
+      <NavigationContainer>
+      <LogoContainer to='/'>
+      <CrwnLogo className='logo' />
+      </LogoContainer>
+      <NavLinks>
+      <NavLink to='/shop'>
+        SHOP
+      </NavLink>
+      
+      {currentUser ? (
+        <NavLink as='span' onClick={signOutUser}>
+          {' '}
+          SIGN OUT{' '}
+        </NavLink>
+      ) : (
+        <NavLink to='/auth'>
+          SIGN IN
+        </NavLink>
+      )}
+      <CartIcon/>
+      </NavLinks>
+      {isCartOpen && <CartDropdown/>}
+        
+      </NavigationContainer>
       <Outlet />
     </Fragment>
   );
 };
 
 export default Navigation;
+
